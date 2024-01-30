e1cbce547fd1b09306be6968c8e798588b61c01d
Oliver Molnar <403access@olimo.me>
2024-01-30 02:53:08 +0100

    install react-native-bottom-sheet and dependencies

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+
+    // react-native-reanimated/plugin has to be listed last
+    // see https://docs.swmansion.com/react-native-reanimated/docs/fundamentals/getting-started/#step-2-add-reanimateds-babel-plugin
+    plugins: ["react-native-reanimated/plugin"],
   };
 };
