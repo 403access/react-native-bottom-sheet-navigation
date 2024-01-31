26748b5006bff47fb7a861ac66e992ff1224c52c
Oliver Molnar <403access@olimo.me>
2024-01-31 14:26:32 +0100

    add themed text with DroidSans

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -3,3 +3,13 @@ import { Text, TextProps } from "./Themed";
 export function MonoText(props: TextProps) {
   return <Text {...props} style={[props.style, { fontFamily: "SpaceMono" }]} />;
 }
+
+export function DroidSansText(props: TextProps) {
+  return <Text {...props} style={[props.style, { fontFamily: "DroidSans" }]} />;
+}
+
+export function DroidSansBoldText(props: TextProps) {
+  return (
+    <Text {...props} style={[props.style, { fontFamily: "DroidSansBold" }]} />
+  );
+}
