21f358d3638b50691ed84c62db96f501791b35a3
Oliver Molnar <403access@olimo.me>
2024-01-31 14:28:19 +0100

    use themed text (ii)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,6 +29,8 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    DroidSans: require("../assets/fonts/DroidSans/DroidSans.ttf"),
+    DroidSansBold: require("../assets/fonts/DroidSans/DroidSans-Bold.ttf"),
     ...FontAwesome.font,
   });
 
