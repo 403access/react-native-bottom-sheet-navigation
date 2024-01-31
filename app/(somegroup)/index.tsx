338ed35ebdc5a6ecc171898a257d2a83acfa8d1c
Oliver Molnar <403access@olimo.me>
2024-01-31 14:29:34 +0100

    use themed text (iii)

--- app/(somegroup)/index.tsx
+++ app/(somegroup)/index.tsx
@@ -1,3 +1,4 @@
+import { DroidSansBoldText, DroidSansText } from "@/components/StyledText";
 import { FontAwesome } from "@expo/vector-icons";
 import { Link } from "expo-router";
 import { Pressable, Text, View } from "react-native";
@@ -5,10 +6,10 @@ import { Pressable, Text, View } from "react-native";
 export default function First() {
   return (
     <View style={{ flex: 1 }}>
-      <Text>first</Text>
+      <DroidSansBoldText>first</DroidSansBoldText>
       <Link href="/(somegroup)/second" asChild>
         <Pressable>
-          <Text>navigate</Text>
+          <DroidSansText>navigate</DroidSansText>
         </Pressable>
       </Link>
     </View>
